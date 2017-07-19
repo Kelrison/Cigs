21b79502e9b0869d2664579779d8e3d012bf14a2
Kelrison <kelrison_coelho@hotmail.com>
2017-07-19 16:06:03 -0400

    Atualização de Dropdown

--- Home.php
+++ Home.php
@@ -3,11 +3,80 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta lang="pt-br">
+        <link rel="stylesheet" type="text/css" href="css/default.css">
         <title>S G D I S</title>
     </head>
-    <body>
-        <?php
-        echo"Página Inicial";
-        ?>
+    
+    <body bgcolor>
+        <header>
+                <!-- Cabeçalho -->            
+        </header>
+        
+        <!-- Menu -->
+        <nav id="menu">
+            <ul id="new_menu">
+                <li> <a href="#"> Opção 1 </a>
+                    <ul>
+                        <li><a href="#">Submenu 01</a></li>
+                        <li><a href="#">Submenu 02</a></li>
+                        <li><a href="#">Submenu 03</a></li>
+                        <li><a href="#">Submenu 04</a></li>
+                                            
+                    </ul>                                
+                </li>                
+                                
+                <li> <a href="#"> Opção 2 </a>
+                <ul>
+                        <li><a href="#">Submenu 01</a></li>
+                        <li><a href="#">Submenu 02</a></li>
+                        <li><a href="#">Submenu 03</a></li>
+                        <li><a href="#">Submenu 04</a></li>
+                                            
+                    </ul>           
+                </li>
+                
+                <li> <a href="#"> Opção 3 </a>
+                <ul>
+                        <li><a href="#">Submenu 01</a></li>
+                        <li><a href="#">Submenu 02</a></li>
+                        <li><a href="#">Submenu 03</a></li>
+                        <li><a href="#">Submenu 04</a></li>
+                                            
+                    </ul>                
+                </li>
+                
+                <li> <a href="#"> Opção 4 </a>
+                <ul>
+                        <li><a href="#">Submenu 01</a></li>
+                        <li><a href="#">Submenu 02</a></li>
+                        <li><a href="#">Submenu 03</a></li>
+                        <li><a href="#">Submenu 04</a></li>
+                                            
+                    </ul>             
+                </li>
+                
+                <li> <a href="#"> Opção 5 </a>
+                <ul>
+                        <li><a href="#">Submenu 01</a></li>
+                        <li><a href="#">Submenu 02</a></li>
+                        <li><a href="#">Submenu 03</a></li>
+                        <li><a href="#">Submenu 04</a></li>
+                                            
+                    </ul>               
+                </li>
+                
+                <li> <a href="#"> Opção 6 </a>
+                <ul>
+                        <li><a href="#">Submenu 01</a></li>
+                        <li><a href="#">Submenu 02</a></li>
+                        <li><a href="#">Submenu 03</a></li>
+                        <li><a href="#">Submenu 04</a></li>
+                                            
+                    </ul>
+                </li>                                
+            </ul>           
+        
+        </nav>
     </body>
 </html>
